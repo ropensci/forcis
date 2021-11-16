ad2f88f05ee4c9a1f60b784255208497eeb6de31
Nicolas Casajus <nicolas.casajus@gmail.com>
2021-11-16 17:22:16 +0100

    Fictive function for access FORCIS database

---
+++ R/get_forcis_db.R
@@ -0,0 +1,22 @@
+#' Download the last version of the FORCIS database
+#'
+#' @description 
+#' ...
+#'
+#' @param path a character. The folder in which the FORCIS database will be save
+#'
+#' @return No return value.
+#' 
+#' @examples
+#' \dontrun{
+#' ## Download the FORCIS database ----
+#' forcis::get_forcis_db()
+#' }
+
+get_forcis_db <- function(path = ".") {
+ 
+  # utils::download.file(url = "url_on_zenodo",
+  #                      destfile = file.path(path, "forcis_database.csv"))
+  
+  invisible(NULL)
+}
