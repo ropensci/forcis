99beaf3e565f9af145eb0c0d6c657d0f994ff0f0
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-05-24 15:30:43 +0200

    Work on select_columns() function

--- R/selects.R
+++ R/selects.R
@@ -1,4 +1,22 @@
+#' Select columns in a data.frame
+#'
+#' @param data a data.frame. One obtained by `get_*_data()` functions.
+#' @param cols a character vector of columns names to keep in addition to the 
+#'   required ones.
+#' 
+#' @export
+#'
+#' @return A `data.frame`
+
 select_columns <- function(data, cols = NULL) {
   
   check_if_not_df(data)
+  
+  if (is.null(cols)) {
+    
+  } else {
+    
+  }
+  
+  data
 }
