7ae5f782f7736babb6971d61df01ec9c7cfa9be9
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-04-17 10:15:09 +0200

    feat: select columns in data

--- R/selects.R
+++ R/selects.R
@@ -1,21 +1,56 @@
-#' Select columns in a data.frame
+#' Select columns in FORCIS data
 #'
-#' @param data a data.frame. One obtained by `get_*_data()` functions.
-#' @param cols a character vector of columns names to keep in addition to the 
+#' @description
+#' __ADD DESCRIPTION__
+#' 
+#' @param data a `data.frame`. One obtained by `get_*_data()` functions.
+#'
+#' @param cols a `character` vector of columns names to keep in addition to the 
 #'   required ones.
 #' 
 #' @export
 #'
-#' @return A `data.frame`
+#' @return A `data.frame`.
+#' 
+#' @examples
+#' ## __ADD EXAMPLE__
 
 select_columns <- function(data, cols = NULL) {
   
+  
+  # Check args ----
+  
   check_if_not_df(data)
+  
+  if (!is.null(cols)) {
+    if (is.character(cols)) {
+      stop("Argument 'cols' must be a character", call. = FALSE)
+    }
+  }
+  
+  
+  # Check for missing columns ----
+  
+  if (any(!(required_columns() %in% colnames(data)))) {
+    stop("Some required columns are absent from data", call. = FALSE)
+  }
+  
+  if (!is.null(cols)) {
+    if (any(!(cols %in% colnames(data)))) {
+      stop("Some columns to select are absent from data", call. = FALSE)
+    }
+  }
+  
+  
+  # Subset columns ----
   
   if (is.null(cols)) {
     
+    data <- data[ , required_columns()]
+    
   } else {
     
+    data <- data[ , c(required_columns(), cols)]
   }
   
   data
