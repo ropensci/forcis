aed85fc514b0739ee1e0564e604d76f9289f5184
Mattia Greco <mattia_greco@outlook.com>
2023-05-12 11:47:37 +0200

    add new conversion functions. Checks for taxonomy and device to be added

--- R/conversions.R
+++ R/conversions.R
@@ -1,4 +1,4 @@
-#' Compute count conversions from net data
+#' Compute count conversions from net and pump data
 #'
 #' @param df forcis net dataset
 #' @param aggregate logical, if FALSE counts will be derived for each subsample. 
@@ -13,89 +13,68 @@
 #' @import messages
 compute_concentrations<-function(df, aggregate=TRUE){
   
+  #insert check device (net and pump only)
+  #insert check taxonomy
   
+  taxa_cols <- extract_species_names(df) 
   
   
   ready_dat<-df %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,.data$profile_id,
-           .data$sample_id,.data$subsample_id,.data$sample_max_depth,
-           .data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>% 
+    select(select(required_columns(),all_of(taxa_cols)),79:276) %>% 
     filter(.data$subsample_count_type=="Absolute") %>%  
-    pivot_longer(11:208, names_to = 'taxa', values_to = 'counts') %>% 
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>%
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep')%>%
-    select(-c(.data$to_drop,.data$subsample_count_type))
+    select(-c(.data$to_drop,.data$subsample_count_type,
+              .data$subsample_all_shells_present_were_counted,
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type))
   
   abs_data_to_convert<-df %>%
     filter(.data$sample_volume_filtered>0) %>% 
-    select(.data$cruise_id,.data$profile_id,
-           .data$new_station_id,.data$profile_id,.data$sample_id,
-           .data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,.data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=='Raw') %>%   
-    pivot_longer(11:208, names_to = 'taxa', values_to ='counts') %>%
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>%
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
     select(-.data$to_drop) %>% 
-    # group_by(sample_id,taxa) %>% 
-    # mutate(counts_tot=sum(counts)) %>%
-    # ungroup() %>% 
     mutate(new_counts=.data$counts/.data$sample_volume_filtered) %>% 
-    select(-c(.data$counts,.data$subsample_count_type)) %>% 
+    select(-c(.data$counts,.data$subsample_count_type,
+              .data$subsample_all_shells_present_were_counted,
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type)) %>% 
     rename('counts'='new_counts') %>% 
     distinct(.data)
   
-  # 
   rel_data_to_convert<-df %>%
     filter(.data$sample_volume_filtered>0) %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,
-           .data$subsample_all_shells_present_were_counted,
-           .data$total_of_forams_counted_ind,79:276) %>%
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=='Relative') %>%
-    pivot_longer(13:210, names_to = 'taxa', values_to = 'counts') %>%
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>%
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>%
     filter(.data$to_drop=='keep') %>%
     select(-.data$to_drop) %>%
     filter(.data$subsample_all_shells_present_were_counted==1) %>%
     filter(!is.na(.data$total_of_forams_counted_ind)) %>%
     mutate(abs_counts=floor((.data$counts*.data$total_of_forams_counted_ind)/ 100)) %>%
-    # group_by(sample_id,taxa) %>%
-    # mutate(counts_tot=sum(counts)) %>%
-    # ungroup() %>%
     mutate(new_counts=.data$abs_counts/.data$sample_volume_filtered) %>%
     select(-c(.data$counts,.data$abs_counts,.data$subsample_count_type,
               .data$subsample_all_shells_present_were_counted,
-              .data$total_of_forams_counted_ind)) %>%
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type)) %>%
     rename('counts'='new_counts') %>%
     distinct(.data)
   
   excluded_samples_volume<-df %>%
     filter(.data$subsample_count_type!="Absolute") %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(is.na(.data$sample_volume_filtered)==TRUE)
   
   excluded_samples_missing_counts<-df %>%
     filter(.data$sample_volume_filtered>0) %>% 
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,
-           .data$subsample_all_shells_present_were_counted,
-           .data$total_of_forams_counted_ind,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=='Relative') %>%    
-    pivot_longer(13:210, names_to = 'taxa', values_to = 'counts') %>%
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>%
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
     select(-.data$to_drop) %>%  
@@ -108,15 +87,15 @@ compute_concentrations<-function(df, aggregate=TRUE){
   
   msg_info("Relative counts from",msg_value(length(unique(excluded_samples_missing_counts$sample_id))),
            "samples could not be converted because of missing data on total assemblage")
-
-
+  
+  
   tot_dat<-rbind(ready_dat,abs_data_to_convert,rel_data_to_convert)
   
-
+  
   if (!aggregate) {
     return(tot_dat)}
   if (aggregate) {
-
+    
     aggregated_dat<-tot_dat %>%
       filter(!is.na(.data$sample_volume_filtered)==TRUE) %>%
       mutate(abs_sub_tot=floor(.data$sample_volume_filtered*.data$counts)) %>%
@@ -129,53 +108,40 @@ compute_concentrations<-function(df, aggregate=TRUE){
       select(-.data$new_counts) %>%
       distinct(.data) %>%
       rename('counts'='conc_counts')
-
+    
     return(aggregated_dat)
   }
-
+  
 }
 
 compute_frequencies<-function(df, aggregate=TRUE){
   
+  #insert check device (net and pump only)
+  #insert check taxonomy
+  
+  taxa_cols <- extract_species_names(df)
+  
   ready_dat<-df %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,.data$subsample_count_type,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=="Relative") %>%    
-    pivot_longer(10:207, names_to = 'taxa', values_to = 'counts') %>%  
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>% 
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
-    select(-c(.data$to_drop,.data$subsample_count_type)) 
+    select(-c(.data$to_drop,.data$subsample_count_type,
+              .data$subsample_all_shells_present_were_counted,
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type,
+              .data$sample_volume_filtered)) 
   
-  samples_to_convert<-df %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$subsample_count_type,79:276) %>%  
-    filter(.data$subsample_count_type!="Relative") %>% 
-    pivot_longer(7:204, names_to = 'taxa', values_to = 'counts') %>% 
-    mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
-    filter(.data$to_drop=='keep') %>%
-    filter(!.data$taxa=='unidentified_specimens') %>%
-    filter(!.data$taxa=='other') %>%
-    filter(!.data$taxa=='reworked_planktic_foraminifera') %>%
-    filter(!.data$taxa=='benthics') %>%
-    group_by(.data$sample_id) %>% 
-    summarise(n_species_counted=n_distinct(.data$taxa)) %>% 
-    ungroup(.data) %>% 
-    filter(.data$n_species_counted>1)
+  samples_to_convert<-df$sample_id[which(df$subsample_all_shells_present_were_counted==1)]
   
-  list_samples<-unique(samples_to_convert$sample_id)
+  list_samples<-unique(samples_to_convert)
   
   
   conc_to_frequency<-df %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,.data$subsample_count_type,
-           .data$sample_volume_filtered,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=="Absolute") %>% 
-    pivot_longer(11:208, names_to = 'taxa', values_to = 'counts') %>% 
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>% 
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
     select(-c(.data$to_drop,.data$subsample_count_type)) %>% 
@@ -190,22 +156,25 @@ compute_frequencies<-function(df, aggregate=TRUE){
     group_by(.data$sample_id) %>% 
     mutate(tot_sample=sum(.data$counts)) %>% 
     ungroup(.data) %>% 
-    select(-.data$sample_volume_filtered)
-  
+    select(-c(.data$sample_volume_filtered,
+              .data$subsample_all_shells_present_were_counted,
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type))
   
   abs_to_frequency<-df %>% 
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,.data$subsample_count_type,79:276) %>%  
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=="Raw") %>% 
-    pivot_longer(10:207, names_to = 'taxa', values_to = 'counts') %>% 
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>% 
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
     select(-c(.data$to_drop,.data$subsample_count_type)) %>% 
     mutate(to_drop=ifelse(.data$sample_id %in% list_samples, 'keep','drop')) %>% 
     filter(.data$to_drop=='keep') %>% 
-    select(- .data$to_drop) %>% 
+    select(- c(.data$to_drop,
+               .data$subsample_all_shells_present_were_counted,
+               .data$total_of_forams_counted_ind,
+               .data$sampling_device_type,
+               .data$sample_volume_filtered)) %>% 
     group_by(.data$subsample_id) %>% 
     mutate(tot_subsample=sum(.data$counts)) %>% 
     ungroup(.data) %>% 
@@ -213,44 +182,25 @@ compute_frequencies<-function(df, aggregate=TRUE){
     mutate(tot_sample=sum(.data$counts)) %>% 
     ungroup(.data)
   
-  
+  #
   merged_frequency<-rbind(conc_to_frequency,abs_to_frequency)
   
   excluded_samples_volume<-df %>% 
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>%  
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=="Absolute") %>% 
-    pivot_longer(8:205, names_to = 'taxa', values_to = 'counts') %>%  
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>% 
     filter(is.na(.data$sample_volume_filtered)==TRUE)
   
   
-  
-  excluded_samples_missing_counts<-df %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>%
-    filter(.data$subsample_count_type!="Relative") %>% 
-    filter(.data$sample_volume_filtered>0) %>% 
-    pivot_longer(8:205, names_to = 'taxa', values_to = 'counts') %>% 
-    mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
-    filter(.data$to_drop=='keep') %>%
-    filter(!.data$taxa=='unidentified_specimens') %>%
-    filter(!.data$taxa=='other') %>%
-    filter(!.data$taxa=='reworked_planktic_foraminifera') %>%
-    filter(!.data$taxa=='benthics') %>%
-    group_by(.data$sample_id) %>% 
-    summarise(n_species_counted=n_distinct(.data$taxa)) %>% 
-    ungroup(.data) %>% 
-    filter(.data$n_species_counted<=1)
+  samples_not_possible__to_convert<-df$sample_id[which(df$subsample_all_shells_present_were_counted==0)]
   
   
   
   msg_info("Counts from",msg_value(length(unique(excluded_samples_volume$sample_id))),
            "samples could not be converted because of missing volume data")
   
-  msg_info("Counts from",msg_value(length(unique(excluded_samples_missing_counts$sample_id))),
-           "samples could not be converted because less then 2 species were identified")
+  msg_info("Counts from",msg_value(length(unique(samples_not_possible__to_convert))),
+           "samples could not be converted because of missing data on total assemblage")
   
   partial_data<-merged_frequency %>% 
     mutate(counts=(.data$counts/.data$tot_subsample)*100) %>% 
@@ -280,34 +230,34 @@ compute_frequencies<-function(df, aggregate=TRUE){
 
 compute_abundances<-function(df, aggregate=TRUE){
   
+  #insert check device (net and pump only)
+  #insert check taxonomy
+  
+  taxa_cols <- extract_species_names(df)
   
   ready_dat<-df %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>%  
     filter(.data$subsample_count_type=="Raw") %>%  
-    pivot_longer(11:208, names_to = 'taxa', values_to = 'counts') %>% 
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>% 
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep')%>%
-    select(-c(.data$to_drop,.data$subsample_count_type))
+    select(-c(.data$to_drop,.data$subsample_count_type,
+              .data$subsample_all_shells_present_were_counted,
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type))
+  
   
   conc_data_to_convert<-df %>%
     filter(.data$sample_volume_filtered>0) %>% 
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>%
     filter(.data$subsample_count_type=='Absolute') %>%   
-    pivot_longer(11:208, names_to = 'taxa', values_to = 'counts') %>%
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>%
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
-    select(-.data$to_drop) %>% 
-    # group_by(sample_id,taxa) %>% 
-    # mutate(counts_tot=sum(counts)) %>%
-    # ungroup() %>% 
+    select(-c(.data$to_drop,
+              .data$sampling_device_type,
+              .data$subsample_all_shells_present_were_counted,
+              .data$total_of_forams_counted_ind)) %>% 
     mutate(new_counts=floor(.data$counts*.data$sample_volume_filtered)) %>% 
     select(-c(.data$counts,.data$subsample_count_type)) %>% 
     rename('counts'='new_counts') %>% 
@@ -316,58 +266,38 @@ compute_abundances<-function(df, aggregate=TRUE){
   
   rel_data_to_convert<-df %>%
     filter(.data$sample_volume_filtered>0) %>% 
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,
-           .data$subsample_all_shells_present_were_counted,
-           .data$total_of_forams_counted_ind,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>%
     filter(.data$subsample_count_type=='Relative') %>%    
-    pivot_longer(13:210, names_to = 'taxa', values_to = 'counts') %>%
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>%
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
     select(-.data$to_drop) %>% 
     filter(.data$subsample_all_shells_present_were_counted==1) %>% 
     filter(!is.na(.data$total_of_forams_counted_ind)) %>% 
     mutate(new_counts=floor((.data$counts*.data$total_of_forams_counted_ind)/ 100)) %>% 
-    # group_by(sample_id,taxa) %>% 
-    # mutate(counts_tot=sum(counts)) %>%
-    # ungroup() %>% 
-    select(-c(.data$counts,.data$subsample_count_type,
+    select(-c(.data$counts,
+              .data$subsample_count_type,
               .data$subsample_all_shells_present_were_counted,
-              .data$total_of_forams_counted_ind)) %>% 
+              .data$total_of_forams_counted_ind,
+              .data$sampling_device_type)) %>% 
     rename('counts'='new_counts') %>% 
     distinct()
   
   excluded_samples_volume<-df %>%
     filter(.data$subsample_count_type!="Raw") %>%
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>%   
     filter(is.na(.data$sample_volume_filtered)==TRUE)
   
-  
-  
+  #
   excluded_samples_missing_counts<-df %>%
     filter(.data$sample_volume_filtered>0) %>% 
-    select(.data$cruise_id,.data$profile_id,.data$new_station_id,
-           .data$profile_id,.data$sample_id,.data$subsample_id,
-           .data$sample_max_depth,.data$site_lat_start_decimal,
-           .data$site_lon_start_decimal,
-           .data$subsample_count_type,.data$sample_volume_filtered,
-           .data$subsample_all_shells_present_were_counted,
-           .data$total_of_forams_counted_ind,79:276) %>% 
+    select(required_columns(),all_of(taxa_cols)) %>% 
     filter(.data$subsample_count_type=='Relative') %>%    
-    pivot_longer(13:210, names_to = 'taxa', values_to = 'counts') %>%
+    pivot_longer(13:ncol(.data), names_to = 'taxa', values_to = 'counts') %>% 
     mutate(to_drop=ifelse(is.na(.data$counts),'drop','keep')) %>% 
     filter(.data$to_drop=='keep') %>% 
     select(-.data$to_drop) %>%  
     filter(is.na(.data$total_of_forams_counted_ind))
-  
-  
   
   msg_info("Counts from",msg_value(length(unique(excluded_samples_volume$sample_id))),
            "samples could not be converted because of missing volume data")
