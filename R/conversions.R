7a806c34f21a8d6882b43722f9dd0251b4f5b90b
Mattia Greco <mattia_greco@outlook.com>
2023-07-13 15:05:37 +0200

    add new compute concentration fun for CPR N

--- R/conversions.R
+++ R/conversions.R
@@ -386,3 +386,27 @@ compute_abundances <- function(data, aggregate = TRUE) {
   
   tot_dat
 }
+
+#' @rdname computations
+#' @export
+
+compute_concentration_bins_CPR_n<- function(data) {
+  
+  ## Check data ----
+  
+  check_if_not_df(data)
+  
+  if (get_data_type(data)!="CPR North") {
+    stop(paste0("This function is designed to work only with CPR North data"), call. = FALSE) 
+  }
+  
+  tot_dat <-data %>% 
+    mutate(min_conc_binned=.data$count_bin_min/.data$sample_volume_filtered,
+           max_conc_binned=.data$count_bin_max/.data$sample_volume_filtered) %>% 
+    select(-c(.data$count_bin_min,
+              .data$count_bin_max))
+  
+  tot_dat
+  
+}
+
