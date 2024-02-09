5b210f7ef268b49b52bf9fa577af8f8e33fef388
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-02-09 09:23:08 +0100

    feat: filter data by ocean - fix #28

---
+++ R/filter_by_ocean.R
@@ -0,0 +1,86 @@
+#' Filter FORCIS data by ocean
+#'
+#' @description
+#' This function can be used to filter FORCIS data by one or several oceans.
+#' 
+#' @param data a `data.frame`, i.e. a FORCIS dataset.
+#' 
+#' @param ocean a `character` vector of one or several ocean names. Use the
+#'   function `get_ocean_names()` to find the correct spelling.
+#'
+#' @return A `data.frame` containing a subset of `data`.
+#' 
+#' @export
+#'
+#' @examples
+#' ## ADD EXAMPLE ----
+
+filter_by_ocean <- function(data, ocean) {
+  
+  ## Check ocean names ----
+  
+  if (missing(ocean)) {
+    stop("Argument 'ocean' is required", call. = FALSE)
+  }
+  
+  if (!is.character(ocean)) {
+    stop("Argument 'ocean' must be a character of length >= 1", call. = FALSE)
+  }
+  
+  if (any(!(ocean %in% get_ocean_names()))) {
+    stop("Some ocean names are mispelled. Please use 'get_ocean_names()' ",
+         "to find the correct spelling", call. = FALSE)
+  }
+  
+  
+  ## Check data object ----
+  
+  if (missing(data)) {
+    stop("Argument 'data' is required", call. = FALSE)
+  }
+  
+  if (!is.data.frame(data)) {
+    stop("Argument 'data' must be a data.frame", call. = FALSE)
+  }
+  
+  if (!("site_lat_start_decimal" %in% colnames(data))) {
+    stop("The column 'site_lat_start_decimal' is missing from 'data'", 
+         call. = FALSE)
+  }
+  
+  if (!("site_lon_start_decimal" %in% colnames(data))) {
+    stop("The column 'site_lon_start_decimal' is missing from 'data'", 
+         call. = FALSE)
+  }
+  
+  
+  ## Convert data into sf object -----
+  
+  data <- data %>% 
+    dplyr::filter(!is.na(.data$site_lat_start_decimal)) %>% 
+    dplyr::filter(!is.na(.data$site_lon_start_decimal))
+  
+  data_sf <- sf::st_as_sf(data, 
+                          coords = c("site_lon_start_decimal", 
+                                     "site_lat_start_decimal"),
+                          crs = sf::st_crs(4326))
+  
+  
+  ## Read IHO layer ----
+  
+  iho <- get_iho_data()
+  iho <- iho[iho$"NAME" %in% ocean, ]
+  
+  
+  ## Project spatial objects into Robinson system ----
+  
+  iho     <- sf::st_transform(iho, sf::st_crs(crs_robinson()))
+  data_sf <- sf::st_transform(data_sf, sf::st_crs(crs_robinson()))
+  
+  
+  ## Spatial filter ----
+  
+  inter <- suppressWarnings(sf::st_intersects(data_sf, iho, sparse = FALSE))
+  
+  data[which(inter), ]
+}
