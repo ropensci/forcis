d0541ce253add83baef2fff71a57aec8e448536a
Nicolas Casajus <rdev.nc@gmail.com>
2025-03-27 15:02:13 +0100

    factor: remove dependency to tidyverse

--- R/compute_concentrations.R
+++ R/compute_concentrations.R
@@ -33,9 +33,6 @@
 #' 
 #' net_data <- read.table(file_name, dec = ".", sep = ";")
 #' 
-#' # Add 'data_type' column ----
-#' net_data$"data_type" <- "Net"
-#' 
 #' # Select a taxonomy ----
 #' net_data <- select_taxonomy(net_data, taxonomy = "VT")
 #' 
@@ -67,14 +64,17 @@ compute_concentrations <- function(data, aggregate = TRUE) {
   
   if (get_data_type(data) == "CPR North") {
       
-    return(
-      data %>% 
-        mutate(min_conc_binned = .data$count_bin_min / 
-                 .data$sample_volume_filtered,
-               max_conc_binned = .data$count_bin_max / 
-                 .data$sample_volume_filtered) %>% 
-        select(-c(.data$count_bin_min,
-                  .data$count_bin_max)))
+    data[["min_conc_binned"]] <- 
+      data$"count_bin_min" / data$"sample_volume_filtered"
+
+    data[["max_conc_binned"]] <- 
+      data$"count_bin_max" / data$"sample_volume_filtered"
+
+    cols_to_remove <- c("count_bin_min", "count_bin_max")
+  
+    data <- data[ , !(colnames(data) %in% cols_to_remove)]
+  
+    return(data)
   }
   
   check_unique_taxonomy(data)
@@ -82,107 +82,169 @@ compute_concentrations <- function(data, aggregate = TRUE) {
   
   taxa_cols <- get_species_names(data) 
   
-  ready_dat <- data %>%
-    filter(.data$subsample_count_type == "Absolute") %>%  
-    pivot_longer(all_of(taxa_cols), 
-                 names_to  = 'taxa', 
-                 values_to = 'counts') %>%
-    mutate(to_drop = ifelse(is.na(.data$counts), 'drop', 'keep')) %>% 
-    filter(.data$to_drop == 'keep') %>%
-    select(-c(.data$to_drop,
-              .data$subsample_count_type,
-              .data$subsample_all_shells_present_were_counted,
-              .data$total_of_forams_counted_ind,
-              .data$sampling_device_type))%>% 
-    rename('counts_n_conc' = 'counts') 
   
-  abs_data_to_convert <- data %>%
-    filter(.data$sample_volume_filtered > 0) %>% 
-    filter(.data$subsample_count_type == 'Raw') %>%   
-    pivot_longer(all_of(taxa_cols), 
-                 names_to  = 'taxa', 
-                 values_to = 'counts') %>%
-    mutate(to_drop = ifelse(is.na(.data$counts), 'drop', 'keep')) %>% 
-    filter(.data$to_drop == 'keep') %>% 
-    select(-.data$to_drop) %>% 
-    mutate(new_counts = .data$counts / .data$sample_volume_filtered) %>% 
-    select(-c(.data$counts,
-              .data$subsample_count_type,
-              .data$subsample_all_shells_present_were_counted,
-              .data$total_of_forams_counted_ind,
-              .data$sampling_device_type)) %>% 
-    rename('counts_n_conc' = 'new_counts') %>% 
-    distinct()
+  ## Absolute data ----
   
-  rel_data_to_convert <- data %>%
-    filter(.data$sample_volume_filtered > 0) %>%
-    filter(.data$subsample_count_type == 'Relative') %>%
-    pivot_longer(all_of(taxa_cols), 
-                 names_to  = 'taxa', 
-                 values_to = 'counts') %>% 
-    mutate(to_drop = ifelse(is.na(.data$counts), 'drop', 'keep')) %>%
-    filter(.data$to_drop == 'keep') %>%
-    select(-.data$to_drop) %>%
-    filter(.data$subsample_all_shells_present_were_counted == 1) %>%
-    filter(!is.na(.data$total_of_forams_counted_ind)) %>%
-    mutate(abs_counts = floor((.data$counts * 
-                                 .data$total_of_forams_counted_ind) / 100)) %>%
-    mutate(new_counts = .data$abs_counts / .data$sample_volume_filtered) %>%
-    select(-c(.data$counts,
-              .data$abs_counts,
-              .data$subsample_count_type,
-              .data$subsample_all_shells_present_were_counted,
-              .data$total_of_forams_counted_ind,
-              .data$sampling_device_type)) %>%
-    rename('counts_n_conc' = 'new_counts') %>%
-    distinct()
+  abs_data <- data[data$"subsample_count_type" == "Absolute", ]
+
+  abs_data <- tidyr::pivot_longer(
+    data      = abs_data, 
+    cols      = tidyr::all_of(taxa_cols), 
+    names_to  = 'taxa', 
+    values_to = 'counts'
+  )
+
+  abs_data <- abs_data[!is.na(abs_data$"counts"), ]
+
+  cols_to_remove <- c("subsample_count_type", "sampling_device_type",
+    "subsample_all_shells_present_were_counted", "total_of_forams_counted_ind"
+  )
+
+  abs_data <- abs_data[ , !(colnames(abs_data) %in% cols_to_remove)]
+
+  colnames(abs_data)[grep("^counts$", colnames(abs_data))] <- 
+    "counts_n_conc"
   
-  excluded_samples_volume <- data %>%
-    filter(.data$subsample_count_type != "Absolute") %>%
-    filter(is.na(.data$sample_volume_filtered))
+
+  ## Raw data ----
+
+  raw_data <- data[data$"subsample_count_type" == "Raw", ]
+  raw_data <- raw_data[raw_data$"sample_volume_filtered" > 0, ]
   
-  excluded_samples_missing_counts <- data %>%
-    filter(.data$sample_volume_filtered > 0) %>% 
-    filter(.data$subsample_count_type == 'Relative') %>%    
-    pivot_longer(all_of(taxa_cols), 
-                 names_to  = 'taxa', 
-                 values_to = 'counts') %>% 
-    mutate(to_drop = ifelse(is.na(.data$counts), 'drop', 'keep')) %>% 
-    filter(.data$to_drop == 'keep') %>% 
-    select(-.data$to_drop) %>%  
-    filter(is.na(.data$total_of_forams_counted_ind))
+  raw_data <- tidyr::pivot_longer(
+    data      = raw_data, 
+    cols      = tidyr::all_of(taxa_cols), 
+    names_to  = "taxa", 
+    values_to = "counts"
+  )
+
+  raw_data <- raw_data[!is.na(raw_data$"counts"), ]
+
+  raw_data[["new_counts"]] <- 
+    raw_data$"counts" / raw_data$"sample_volume_filtered"
+
+  cols_to_remove <- c("counts", "subsample_count_type", "sampling_device_type",
+    "subsample_all_shells_present_were_counted", "total_of_forams_counted_ind"
+  )
+
+  raw_data <- raw_data[ , !(colnames(raw_data) %in% cols_to_remove)]
+
+  colnames(raw_data)[grep("^new_counts$", colnames(raw_data))] <- 
+    "counts_n_conc"
   
-  message("Counts from ", 
-          length(unique(excluded_samples_volume$"sample_id")),
-          " samples could not be converted because of missing volume data")
+  raw_data <- raw_data[!duplicated(raw_data), ]
+
   
-  message("Relative counts from ", 
-          length(unique(excluded_samples_missing_counts$"sample_id")),
-          " samples could not be converted because of missing data on total ",
-          "assemblage")
+  ## Relative data ----
+
+  rel_data <- data[data$"sample_volume_filtered" > 0, ]
+  rel_data <- rel_data[rel_data$"subsample_count_type" == "Relative", ]
+
+  rel_data <- tidyr::pivot_longer(
+    data      = rel_data, 
+    cols      = tidyr::all_of(taxa_cols), 
+    names_to  = 'taxa', 
+    values_to = 'counts'
+  )
+
+  rel_data <- rel_data[!is.na(rel_data$"counts"), ]
+
+  rel_data <- rel_data[
+    rel_data$"subsample_all_shells_present_were_counted" == 1, ]
+
+  rel_data <- rel_data[!is.na(rel_data$"total_of_forams_counted_ind"), ]
+
+  rel_data[["abs_counts"]] <- floor(
+    (rel_data$"counts" * rel_data$"total_of_forams_counted_ind") / 100
+  )
+
+  rel_data[["new_counts"]] <- 
+    rel_data$"abs_counts" / rel_data$"sample_volume_filtered"
+
+  cols_to_remove <- c(
+    "counts", "abs_counts", "subsample_count_type",
+    "subsample_all_shells_present_were_counted", "total_of_forams_counted_ind",
+    "sampling_device_type"
+  )
+
+  rel_data <- rel_data[ , !(colnames(rel_data) %in% cols_to_remove)]
+
+  colnames(rel_data)[grep("^new_counts$", colnames(rel_data))] <- 
+    "counts_n_conc"
+
+  rel_data <- rel_data[!duplicated(rel_data), ]
+
+
+  ## Compute metrics for messages ----
+
+  missing_volume <- data[data$"subsample_count_type" != "Absolute", ]
+  missing_volume <- missing_volume[
+    is.na(missing_volume$"sample_volume_filtered"), ]
+
+   missing_volume <- length(unique(missing_volume$"sample_id"))
   
-  tot_dat <- rbind(ready_dat,
-                   abs_data_to_convert,
-                   rel_data_to_convert)
+   missing_counts <- data[data$"sample_volume_filtered" > 0, ]
+   missing_counts <- missing_counts[
+     missing_counts$"subsample_count_type" == "Relative", ]
+   
+   missing_counts <- tidyr::pivot_longer(
+     data      = missing_counts, 
+     cols      = tidyr::all_of(taxa_cols), 
+     names_to  = "taxa", 
+     values_to = "counts"
+   )
+ 
+   missing_counts <- missing_counts[!is.na(missing_counts$"counts"), ]
+   missing_counts <- missing_counts[
+     is.na(missing_counts$"total_of_forams_counted_ind"), ]
+ 
+   missing_counts <- length(unique(missing_counts$"sample_id"))
+ 
+  
+  message(
+    "Counts from ", missing_volume, " samples could not be converted ", 
+    "because of missing volume data"
+  )
+
+  message(
+    "Relative counts from ", missing_counts, " samples could not be ", 
+    "converted because of missing data on total assemblage"
+  )
+  
+  tot_data <- rbind(raw_data, abs_data, rel_data)
+  
   
   if (aggregate) {
+
+    tot_data <- tot_data[!is.na(tot_data$"sample_volume_filtered"), ]
     
-    tot_dat <- tot_dat %>%
-      filter(!is.na(.data$sample_volume_filtered)) %>%
-      mutate(abs_sub_tot = floor(.data$sample_volume_filtered * 
-                                   .data$counts_n_conc)) %>%
-      group_by(.data$sample_id, .data$taxa) %>%
-      mutate(new_counts = sum(.data$abs_sub_tot, na.rm = TRUE)) %>%
-      ungroup() %>%
-      select(-c(.data$counts_n_conc, .data$abs_sub_tot, .data$subsample_id,
-                .data$subsample_size_fraction_min,
-                .data$subsample_size_fraction_max)) %>%
-      distinct() %>%
-      mutate(conc_counts = .data$new_counts / .data$sample_volume_filtered) %>%
-      select(-.data$new_counts) %>%
-      distinct() %>%
-      rename('counts_n_conc' = 'conc_counts')
+    tot_data[["abs_sub_tot"]] <- floor(
+      tot_data$"sample_volume_filtered" * tot_data$"counts_n_conc"
+    )
+
+    cols_to_remove <- c(
+      "counts_n_conc", "abs_sub_tot", "subsample_id", 
+      "subsample_size_fraction_min", "subsample_size_fraction_max", "taxa"
+    )
+  
+    sample_data <- tot_data[ , !(colnames(tot_data) %in% cols_to_remove)]
+    sample_data <- sample_data[!duplicated(sample_data), ]
+
+    y <- stats::aggregate(
+      abs_sub_tot ~ sample_id + taxa, 
+      data = tot_data, function(x) sum(x, na.rm = TRUE)
+    )
+
+    tot_data <- merge(sample_data, y, by = "sample_id")
+
+    tot_data[["abs_sub_tot"]] <- 
+      tot_data$"abs_sub_tot" / tot_data$"sample_volume_filtered"
+
+    colnames(tot_data)[grep("^abs_sub_tot$", colnames(tot_data))] <- 
+      "counts_n_conc"
+
+    tot_data <- tot_data[c(colnames(sample_data), "taxa", "counts_n_conc")]
   }
   
-  tot_dat
+  tibble::as_tibble(tot_data)
 }
